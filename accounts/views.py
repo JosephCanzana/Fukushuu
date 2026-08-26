1a60bab91be135ec046e88c0b4e117f02f52a478
Joseph Canzana <canzanajoseph@gmail.com>
2026-08-26 08:54:28 +0800

    Update: Sign-up.html

--- accounts/views.py
+++ accounts/views.py
@@ -7,5 +7,12 @@ def landing(request):
 
 
 def sign_up(request):
-    return HttpResponse("Sign up!")
+    if request == "POST":
+        # Here you can print the request data to check if it's being submitted correctly
+        print(request.POST)
+        
+        # You can also return a simple HTTP response with a success message
+        return HttpResponse('Form submitted successfully', status=200)
+
+    return render(request, "accounts/sign_up.html")
 
